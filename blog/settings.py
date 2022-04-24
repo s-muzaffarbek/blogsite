0625f2774896777db372b52f4e3159eb0582a777
Muzaffarbek <88822010+s-muzaffarbek@users.noreply.github.com>
2022-04-25 00:50:36 +0500

    update email

--- blog/settings.py
+++ blog/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'muzaffarbeksaidahmedov@gmail.com'
-EMAIL_HOST_PASSWORD = 'muzaffarbek1109'
+EMAIL_HOST_PASSWORD = 'email-password'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
